be611590d4831857102f473a32d20b3fe4c296c2
MarkusSchoedl <32414751+MarkusSchoedl@users.noreply.github.com>
2018-09-14 10:28:44 +0200

    Added logging

--- ScreensaverService/Service1.cs
+++ ScreensaverService/Service1.cs
@@ -2,20 +2,24 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
+using System.Reflection;
 using System.ServiceProcess;
 
 namespace ScreensaverService
 {
     public partial class Service1 : ServiceBase
     {
+        #region Fields
         private string _winpath = @"C:\Users\marku\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\";
         private string _picpath = @"C:\Users\marku\Pictures\WinBackgrounds";
         private string _picExt = "jpg";
-        private string[] _files;
 
-        private List<Size> _applicablePictureSizes = new List<Size>();
+        private readonly List<Size> _applicablePictureSizes = new List<Size>();
 
-        private FileSystemWatcher _fileWatcher = new FileSystemWatcher();
+        private readonly FileSystemWatcher _fileWatcher = new FileSystemWatcher();
+
+        private readonly log4net.ILog _log = log4net.LogManager.GetLogger(Assembly.GetExecutingAssembly().FullName);
+        #endregion
 
         public Service1()
         {
@@ -28,6 +32,8 @@ namespace ScreensaverService
 
         protected override void OnStart(string[] args)
         {
+            _log.Info("======== Starting up Service ========");
+
             _fileWatcher.Path = _winpath;
             _fileWatcher.Created += PossibleBackgroundCreated;
             _fileWatcher.EnableRaisingEvents = true;
@@ -35,6 +41,8 @@ namespace ScreensaverService
 
         private void PossibleBackgroundCreated(object sender, FileSystemEventArgs e)
         {
+            _log.Info("New File arrived.");
+
             string sourcePath = Path.Combine(_fileWatcher.Path, e.Name);
             Image img;
 
@@ -42,19 +50,23 @@ namespace ScreensaverService
             {
                 img = Image.FromFile(sourcePath);
             }
-            catch (OutOfMemoryException ex)
+            catch (OutOfMemoryException)
             {
                 // A new File was found but was not in the format of an image.
+                _log.Warn("The new File was not in the format of an image. Name: " + e.Name);
                 return;
             }
             catch (Exception ex)
             {
-                // derfuq
+                _log.Error("An unknown Exception was thrown. Name: " + e.Name + " - Exception: " + ex);
                 return;
             }
-            
+
+            _log.Info("Its an image with a size of " + img.Size.Width + "x" + img.Size.Height);
+
             if (_applicablePictureSizes.Contains(img.Size))
             {
+                _log.Info("Matching Size. Copying picture to the users folder.");
                 string destPath = Path.Combine(_picpath, e.Name) + "." + _picExt;
                 File.Copy(sourcePath, destPath, true);
             }
